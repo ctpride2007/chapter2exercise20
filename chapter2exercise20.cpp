d1b36172f0da0f037a5660eaafdb2ddd9344cfc9
ctpride07 <ctpride07@gmail.com>
2025-09-07 13:02:42 -0400

    Add project files.

---
+++ chapter2exercise20.cpp
@@ -0,0 +1,13 @@
+/*
+* File: chapter2exercise20
+* Programmer: Carter Pride
+* Date: 9/25
+* Purpose: Write a program to determine and report approximately how many gallons of paint will be needed to paint two coats on a wooden fence that is 6 feet high and 100 feet long
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
